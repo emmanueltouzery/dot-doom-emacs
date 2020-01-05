a1b46ecb5ac9820665fd22d7b9f9670c38ee27e1
Emmanuel Touzery <etouzery@gmail.com>
2020-01-05 10:16:12 +0100

    ability to switch between windows using `spc num` as in spacemacs, thanks Henrik from the doom-emacs discord!

--- config.el
+++ config.el
@@ -48,3 +48,17 @@
 ;;
 ;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
 ;; they are implemented.
+
+(map! :leader
+      "0" #'winum-select-window-0-or-10
+      "1" #'winum-select-window-1
+      "2" #'winum-select-window-2
+      "3" #'winum-select-window-3
+      "4" #'winum-select-window-4
+      "5" #'winum-select-window-5
+      "6" #'winum-select-window-6
+      "7" #'winum-select-window-7
+      "8" #'winum-select-window-8
+      "9" #'winum-select-window-9)
+(map! :leader "a" #'evil-switch-to-windows-last-buffer)
+
